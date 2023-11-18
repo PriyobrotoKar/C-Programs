35d77fdbf3414e1b8d6182e0fba995db758597ae
Priyobroto Kar <priyobrotokar@gmail.com>
2023-11-18 11:52:21 +0530

    fixed leap year problem

--- LeapYear.c
+++ LeapYear.c
@@ -6,7 +6,7 @@ void main()
     int year;
     printf("Enter a year:");
     scanf("%d", &year);
-    if (!(year % 4 || year % 400) || year % 100)
+    if (!(year % 4) && year % 100 || !(year % 400))
     {
         printf("%d is a leap year", year);
     }
